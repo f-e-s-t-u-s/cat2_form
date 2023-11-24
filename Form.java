17a2d3310692a6f611505304d3ff82854c78a9fb
Festus Gitahi <festusgitahik@gmail.com>
2023-11-24 14:21:21 +0300

    added cat2

---
+++ Form.java
@@ -0,0 +1,171 @@
+// package dev.clint;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+import javax.swing.JPasswordField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Form {
+
+	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_4;
+	private JTextField textField_5;
+	private JTextField textField_6;
+	private JPasswordField pwdPassword;
+	private JPasswordField pwdConfirm;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Form window = new Form();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Form() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 558, 533);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblRegisterForm = new JLabel("Register Form");
+		lblRegisterForm.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		lblRegisterForm.setBounds(15, 16, 234, 37);
+		frame.getContentPane().add(lblRegisterForm);
+		
+		textField = new JTextField();
+		textField.setBounds(181, 78, 339, 26);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(181, 120, 339, 26);
+		frame.getContentPane().add(textField_1);
+		
+		textField_4 = new JTextField();
+		textField_4.setColumns(10);
+		textField_4.setBounds(181, 248, 339, 26);
+		frame.getContentPane().add(textField_4);
+		
+		textField_5 = new JTextField();
+		textField_5.setColumns(10);
+		textField_5.setBounds(181, 291, 339, 26);
+		frame.getContentPane().add(textField_5);
+		
+		textField_6 = new JTextField();
+		textField_6.setColumns(10);
+		textField_6.setBounds(181, 333, 339, 26);
+		frame.getContentPane().add(textField_6);
+		
+		JLabel lblName = new JLabel("Name :");
+		lblName.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblName.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblName.setBounds(15, 81, 151, 20);
+		frame.getContentPane().add(lblName);
+		
+		JLabel lblUsername = new JLabel("Username :");
+		lblUsername.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblUsername.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblUsername.setBounds(15, 123, 151, 20);
+		frame.getContentPane().add(lblUsername);
+		
+		JLabel lblConfirmPassword = new JLabel("Confirm Password :");
+		lblConfirmPassword.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblConfirmPassword.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblConfirmPassword.setBounds(15, 209, 151, 20);
+		frame.getContentPane().add(lblConfirmPassword);
+		
+		JLabel lblEmail = new JLabel("Email :");
+		lblEmail.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblEmail.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblEmail.setBounds(15, 251, 151, 20);
+		frame.getContentPane().add(lblEmail);
+		
+		JLabel lblPhone = new JLabel("Phone :");
+		lblPhone.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblPhone.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblPhone.setBounds(15, 294, 151, 20);
+		frame.getContentPane().add(lblPhone);
+		
+		JLabel lblAddress = new JLabel("Address :");
+		lblAddress.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblAddress.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblAddress.setBounds(15, 336, 151, 20);
+		frame.getContentPane().add(lblAddress);
+		
+		JLabel lblPassword = new JLabel("Password :");
+		lblPassword.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblPassword.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblPassword.setBounds(15, 165, 151, 20);
+		frame.getContentPane().add(lblPassword);
+		
+		JButton btnSubmit = new JButton("Submit");
+		btnSubmit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		btnSubmit.setBounds(181, 402, 90, 29);
+		frame.getContentPane().add(btnSubmit);
+		
+		JButton btnClose = new JButton("Close");
+		btnClose.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent a) {
+				System.exit(0);
+			}
+		});
+		btnClose.setBounds(430, 402, 90, 29);
+		frame.getContentPane().add(btnClose);
+		
+		JButton btnReset = new JButton("Reset");
+		btnReset.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textField.setText("");
+				textField_1.setText("");
+				pwdPassword.setText("");
+				pwdConfirm.setText("");
+				textField_4.setText("");
+				textField_5.setText("");
+				textField_6.setText("");
+			}
+		});
+		btnReset.setBounds(300, 402, 103, 29);
+		frame.getContentPane().add(btnReset);
+		
+		pwdPassword = new JPasswordField();
+		pwdPassword.setBounds(181, 162, 339, 26);
+		frame.getContentPane().add(pwdPassword);
+		
+		pwdConfirm = new JPasswordField();
+		pwdConfirm.setBounds(181, 206, 339, 26);
+		frame.getContentPane().add(pwdConfirm);
+	}
+}
